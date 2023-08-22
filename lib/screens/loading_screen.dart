4189a4b5f933dc3989a7db0f10b236462e879334
meruem243 <mughees.ahmed243@gmail.com>
2023-08-23 02:07:16 +0500

    initializing

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text(
+            'Get Location',
+          ),
+        ),
+      ),
+    );
+  }
+}
